608512e5bc0623a814e499cd066c2bc72134e63a
Marcel Siegert <mws@cvs.tuxbox.org>
2005-11-10 23:55:32 +0000

    added 7 new descriptors to libdvbsi++; changed the satellite delivery system descriptor to support dvb-s2 (information therefore taken from dvbsnoop - thanks) will be verified after final release of EN 300468 v1.7.1

---
+++ src/related_content_descriptor.cpp
@@ -0,0 +1,28 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2005 Marcel Siegert <mws@twisted-brains.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * See the file 'COPYING' in the top level directory for details.
+ */
+#include "dvbsi++/related_content_descriptor.h"
+
+RelatedContentDescriptor::RelatedContentDescriptor(const uint8_t* const buffer) : Descriptor(buffer),
+	bytes(descriptorLength)
+{
+
+	memcpy(&bytes[0], buffer+2, descriptorLength);
+}
+
+RelatedContentDescriptor::~RelatedContentDescriptor()
+{
+}
+
+const RelatedContentByteVector* RelatedContentDescriptor::getRelatedContentBytes(void) const
+{
+	return &bytes;
+}
