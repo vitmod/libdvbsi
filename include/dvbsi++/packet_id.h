efee53a9c142592a47df450b0e0a5ba99d9085e3
Andreas Oberritter <obi@cvs.tuxbox.org>
2009-06-25 16:29:01 +0000

    update identifiers to ETSI EN 300 468 V1.9.1

--- include/dvbsi++/packet_id.h
+++ include/dvbsi++/packet_id.h
@@ -14,18 +14,20 @@
 #define __packet_id_h__
 
 enum PacketId {
-	/* ETSI EN 300 468 V1.5.1 (2003-05) */
+	/* ETSI EN 300 468 V1.9.1 (2009-03) */
 	PID_PAT		= 0x0000,
 	PID_CAT		= 0x0001,
 	PID_TSDT	= 0x0002,
 	PID_NIT		= 0x0010,
 	PID_BAT		= 0x0011,
 	PID_SDT		= 0x0011,
+	PID_CIT		= 0x0012,	/* TS 102 323 */
 	PID_EIT		= 0x0012,
 	PID_RST		= 0x0013,
 	PID_TDT		= 0x0014,
 	PID_TOT		= 0x0014,
 	PID_NS		= 0x0015,	/* network synchronization */
+	PID_RNT		= 0x0016,	/* TS 102 323 */
 	PID_IS		= 0x001C,	/* inband signaling (SIS-12) */
 	PID_M		= 0x001D,	/* measurement (SIS-10) */
 	PID_DIT		= 0x001E,
--- include/dvbsi++/service_type.h
+++ include/dvbsi++/service_type.h
@@ -13,7 +13,7 @@
 #define __service_type_h__
 
 enum ServiceType {
-	/* 0x00 - 0x1B: ETSI EN 300 468 V1.7.1 (2006-05)*/
+	/* 0x00 - 0x1B: ETSI EN 300 468 V1.9.1 (2009-03)*/
 	ST_RESERVED				= 0x00,
 	ST_DIGITAL_TELEVISION_SERVICE		= 0x01,
 	ST_DIGITAL_RADIO_SOUND_SERVICE		= 0x02,
